353dba85b704d438a4f1e3484cfd1d74bbf59b83
Alamin-22 <mdalaminmollick@gmail.com>
2024-11-20 22:38:41 +0600

    setting up Backend

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
-//parsers
+import cors from 'cors';
+
+// parser
+
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello World! er maire bap');
 });
-
-console.log(process.cwd());
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose';
-import config from './app/config';
+import 'dotenv/config';
 import app from './app';
+import config from './app/config';
 
 async function main() {
-  try {
-    await mongoose.connect(config.dataBaseUrl as string);
+  await mongoose.connect(config.dataBaseUrl as string);
 
+  try {
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(
+        `First Professional Backend is running on port =>  ${config.port}`,
+      );
     });
   } catch (error) {
     console.log(error);
   }
 }
+
+main();
