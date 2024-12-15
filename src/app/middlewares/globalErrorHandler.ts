a8b76a92ccae42fb854857eaec13a7a9cdea4107
Alamin-22 <mdalaminmollick@gmail.com>
2024-12-15 22:58:50 +0600

    working on to create custom Error type

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,22 +2,54 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interfaces/error';
+import config from '../config';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const statusCode = 400;
+
+    const errorSource: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSource,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+
+    console.log(simplifiedError);
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
-  });
+    errorSource,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+  }) as unknown as void;
 };
 
 export default globalErrorHandler;
