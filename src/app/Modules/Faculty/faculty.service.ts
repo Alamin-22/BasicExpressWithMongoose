41d564cac62de8045c03f5cf349c2c44e7cd6934
Alamin-22 <mdalaminmollick@gmail.com>
2024-12-18 08:19:55 +0600

    working on the Faculty

---
+++ src/app/Modules/Faculty/faculty.service.ts
@@ -0,0 +1,8 @@
+// import QueryBuilder from "../../builder/QueryBuilder"
+// import { Faculty } from "./faculty.model"
+
+//  const getAllFacultiesFromDB = async (query:Record<string, unknown>)=>{
+//      const facultyQuery= new QueryBuilder(
+//          Faculty.
+//      )
+//  }
