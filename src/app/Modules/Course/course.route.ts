e34fdbbab1a546fe1def659f86878cb393db6f7a
Alamin-22 <mdalaminmollick@gmail.com>
2024-12-28 18:35:57 +0600

    created basic courses related files

---
+++ src/app/Modules/Course/course.route.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { CourseControllers } from './course.controller';
+import { CourseValidations } from './course.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create_course',
+  validateRequest(CourseValidations.createCourseValidationSchema),
+  CourseControllers.createCourse,
+);
+
+router.get('/:id', CourseControllers.getSingleCourse);
+
+router.patch(
+  '/:id',
+  validateRequest(CourseValidations.updateCourseValidationSchema),
+  CourseControllers.updateCourse,
+);
+
+router.delete('/:id', CourseControllers.deleteCourse);
+
+router.put(
+  '/:courseId/assign_faculties',
+  validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
+  CourseControllers.assignFacultiesWithCourse,
+);
+
+router.delete(
+  '/:courseId/remove_faculties',
+  validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
+  CourseControllers.removeFacultiesFromCourse,
+);
+
+router.get('/', CourseControllers.getAllCourses);
+
+export const CourseRoutes = router;
