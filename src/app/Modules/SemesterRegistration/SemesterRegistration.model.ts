195608374d4a3cc884745b4d404523b0447d57c7
Alamin-22 <mdalaminmollick@gmail.com>
2025-01-17 20:31:50 +0600

    created Semester Registration Validaiton and model

--- src/app/Modules/SemesterRegistration/SemesterRegistration.model.ts
+++ src/app/Modules/SemesterRegistration/SemesterRegistration.model.ts
@@ -1,8 +1,37 @@
 import { model, Schema } from 'mongoose';
 import { TSemesterRegistration } from './SemesterRegistration.interface';
+import { semesterRegistrationStatus } from './SemesterRegistration.constant';
 
 const semesterRegistrationSchema = new Schema<TSemesterRegistration>(
-  {},
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      unique: true,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: semesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+    },
+  },
   {
     timestamps: true,
   },
@@ -25,6 +54,7 @@ semesterRegistrationSchema.pre('aggregate', function (next) {
 });
 //
 
-export const SemesterRegistrationModel= model<TSemesterRegistration>("SemesterRegistrations",semesterRegistrationSchema)
-
-
+export const SemesterRegistrationModel = model<TSemesterRegistration>(
+  'SemesterRegistrations',
+  semesterRegistrationSchema,
+);
