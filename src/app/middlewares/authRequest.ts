55caa3eb0df7d29bcb3cc6a39d7d2fe7baba5213
Alamin-22 <mdalaminmollick@gmail.com>
2025-01-21 23:27:08 +0600

    added authorization logic

--- src/app/middlewares/authRequest.ts
+++ src/app/middlewares/authRequest.ts
@@ -4,11 +4,11 @@ import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+import { TUser } from '../Modules/User/user.interface';
 
-const AuthValidationMiddleWare = () => {
+const AuthValidationMiddleWare = (...requiredRoles: TUser[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
-    console.log({ token });
 
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'Your Are Unauthorized!');
@@ -19,8 +19,15 @@ const AuthValidationMiddleWare = () => {
       if (err) {
         throw new AppError(httpStatus.UNAUTHORIZED, 'Your Are Unauthorized!');
       }
+
+      const userRole = (decoded as JwtPayload)?.role;
+
+      // checking the Role of the User
+      if (requiredRoles && !requiredRoles.includes(userRole)) {
+        throw new AppError(httpStatus.UNAUTHORIZED, 'Your Are Unauthorized!');
+      }
+
       // decoded undefined
-      console.log({ decoded });
       req.user = decoded as JwtPayload;
       next();
     });
