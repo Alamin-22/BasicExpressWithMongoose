d03f7faaade18c5f90b4735feb2c36c7136a0bd8
Alamin-22 <mdalaminmollick@gmail.com>
2025-01-24 11:23:27 +0600

    resolved the issue with Type for User Role

--- src/app/Modules/Auth/auth.service.ts
+++ src/app/Modules/Auth/auth.service.ts
@@ -123,12 +123,16 @@ const changePassword = async (
 };
 
 const refreshToken = async (token: string) => {
-  
   // checking if the given token is valid
   const decoded = jwt.verify(
     token,
     config.refresh_secret as string,
   ) as JwtPayload;
+  console.log(
+    'This is coming from the Auth Service 131 line',
+    config.refresh_secret,
+    { decoded },
+  );
 
   const { userId, iat } = decoded;
 
@@ -172,6 +176,12 @@ const refreshToken = async (token: string) => {
     config.access_secret as string,
     config.jwt_access_expire_In as string,
   );
+
+  // const accessToken = createToken(
+  //   jwtPayload,
+  //   config.jwt_access_secret as string,
+  //   config.jwt_access_expires_in as string,
+  // );
 
   return {
     accessToken,
