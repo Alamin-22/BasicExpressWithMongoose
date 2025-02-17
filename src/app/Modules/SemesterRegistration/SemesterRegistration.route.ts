2b859e6958a52331c2355983341fffc08c45c3aa
Alamin-22 <mdalaminmollick@gmail.com>
2025-02-18 01:01:20 +0600

    modifed the semester Regestration logiv

--- src/app/Modules/SemesterRegistration/SemesterRegistration.route.ts
+++ src/app/Modules/SemesterRegistration/SemesterRegistration.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { SemesterRegistrationControllers } from './SemesterRegistration.controller';
 import ValidateRequestMiddleWare from '../../middlewares/validateRequest';
 import { SemesterRegistrationValidations } from './SemesterRegistration.validation';
+import AuthValidationMiddleWare from '../../middlewares/authRequest';
+import { USER_ROLE } from '../User/user.constant';
 
 const router = express.Router();
 // crate Registration
 router.post(
   '/create_semester_registration',
+  AuthValidationMiddleWare(USER_ROLE.admin, USER_ROLE.supper_admin),
   ValidateRequestMiddleWare(
     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
@@ -14,16 +17,22 @@ router.post(
 );
 
 // get all Registered info
-router.get('/', SemesterRegistrationControllers.getAllSemesterRegistrations);
+router.get(
+  '/',
+  AuthValidationMiddleWare(USER_ROLE.admin, USER_ROLE.supper_admin),
+  SemesterRegistrationControllers.getAllSemesterRegistrations,
+);
 
 // get Single Registered info
 router.get(
   '/:id',
+  AuthValidationMiddleWare(USER_ROLE.admin, USER_ROLE.supper_admin),
   SemesterRegistrationControllers.getSingleSemesterRegistration,
 );
 // UPdate Single Registered info
 router.patch(
   '/:id',
+  AuthValidationMiddleWare(USER_ROLE.admin, USER_ROLE.supper_admin),
   ValidateRequestMiddleWare(
     SemesterRegistrationValidations.updateSemesterRegistrationValidationSchema,
   ),
--- src/app/Modules/academicSemester/academicSemester.route.ts
+++ src/app/Modules/academicSemester/academicSemester.route.ts
@@ -2,28 +2,47 @@ import express from 'express';
 import { academicSemesterControllers } from './academicSemester.controller';
 import ValidateRequestMiddleWare from '../../middlewares/validateRequest';
 import { AcademicSemesterValidations } from './academicSemesterValidation';
+import AuthValidationMiddleWare from '../../middlewares/authRequest';
+import { USER_ROLE } from '../User/user.constant';
 const router = express.Router();
 
 // create a middleware
 
 router.post(
   '/create_academic_semester',
+  AuthValidationMiddleWare(USER_ROLE.admin, USER_ROLE.supper_admin),
   ValidateRequestMiddleWare(
     AcademicSemesterValidations.createAcademicSemesterValidationSchema,
   ),
   academicSemesterControllers.createAcademicSemester,
 );
 // get all semester
-router.get('/', academicSemesterControllers.getAllAcademicSemester);
+router.get(
+  '/',
+  AuthValidationMiddleWare(
+    USER_ROLE.admin,
+    USER_ROLE.supper_admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  academicSemesterControllers.getAllAcademicSemester,
+);
 // get single semester
 router.get(
   '/:semesterId',
+  AuthValidationMiddleWare(
+    USER_ROLE.admin,
+    USER_ROLE.supper_admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   academicSemesterControllers.getSingleAcademicSemester,
 );
 
 // update single semester
 router.patch(
   '/:semesterId',
+  AuthValidationMiddleWare(USER_ROLE.admin, USER_ROLE.supper_admin),
   ValidateRequestMiddleWare(
     AcademicSemesterValidations.updateAcademicSemesterValidationSchema,
   ),
